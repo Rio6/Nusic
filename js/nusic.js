c39a09df64ad3684e615b68c44ed0692de4c7f22
Rio6 <rio6.liu@gmail.com>
2019-12-12 13:46:20 -0800

    Some more scales

--- js/nusic.js
+++ js/nusic.js
@@ -7,6 +7,11 @@ var scales = {
     Mixolydian: [0, 2, 4, 5, 7, 9, 10],
     Aeolian: [0, 2, 3, 5, 7, 8, 10],
     Locrian: [0, 1, 3, 5, 6, 8, 10],
+    "Pentatonic": [0, 2, 4, 7, 9],
+    "Minor Pentatonic": [0, 3, 5, 7, 10],
+    Hexatonic: [0, 3, 5, 6, 7, 10],
+    Heptatonic: [0, 2, 3, 5, 6, 9, 10],
+    Nonatonic: [0, 2, 3, 4, 5, 7, 9, 10, 11],
     Chromatic: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
 };
 
