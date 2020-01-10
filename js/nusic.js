a04dfffa68ba05b4761c518f2e5897e2f22ea3f8
Rio6 <rio6.liu@gmail.com>
2020-01-10 12:42:17 -0800

    More scales

--- js/nusic.js
+++ js/nusic.js
@@ -9,8 +9,10 @@ var scales = {
     Locrian: [0, 1, 3, 5, 6, 8, 10],
     "Pentatonic": [0, 2, 4, 7, 9],
     "Minor Pentatonic": [0, 3, 5, 7, 10],
-    Hexatonic: [0, 3, 5, 6, 7, 10],
+    Blues: [0, 3, 5, 6, 7, 10],
+    "Whole Tone": [0, 2, 4, 6, 8, 10],
     Heptatonic: [0, 2, 3, 5, 6, 9, 10],
+    Diminished: [0, 2, 3, 5, 6, 8, 9, 11],
     Nonatonic: [0, 2, 3, 4, 5, 7, 9, 10, 11],
     Chromatic: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
 };
